8408d3c8c0b04e59d867ef935ea669589f741a9c
Oleksandr Chyzh <oleksandr.chyzh@soft-loft.eu>
2024-08-17 17:30:58 +0300

    init

---
+++ Weather_SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather_SwiftUI
+//
+//  Created by Oleksandr Chyzh on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
